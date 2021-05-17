f19eb39666bc500c5d9f29e9c1537ba77bbc5a04
econom1st <camcycamovu4@gmail.com>
2021-05-17 10:40:29 +0300

    add spinner and content

---
+++ src/components/errorMessage/errorMessage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ErrorMessage = () => {
+    return <span>Something goes wrong</span>
+}
+
+export default ErrorMessage;
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-spin-6s8vsocvj8t"><div class="ldio-04mrtysve73j">
+            <div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div>
+        </div></div>
+    )
+}
+
+export default Spinner;
+
+
+
