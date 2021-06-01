fe6e25e0f60ac8b45a28f0411fae04576be2c405
econom1st <camcycamovu4@gmail.com>
2021-06-01 19:15:29 +0300

    add booksItem

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,7 +3,7 @@ import {Col, Row, Container} from 'reactstrap';
 import Header from '../header';
 import RandomChar from '../randomChar';
 import ErrorMessage from '../errorMessage';
-import {CharacterPage, BooksPage, HousesPage} from '../pages';
+import {CharacterPage, BooksPage, HousesPage, BooksItem} from '../pages';
 import gotService from '../../services/gotService';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 
@@ -51,7 +51,16 @@ export default class App extends Component {
                         <Route path='/' exact component={() => <h1>Welcome to GOT DB</h1> } />
                         <Route path='/characters' component={CharacterPage} />
                         <Route path='/houses' component={HousesPage} />
-                        <Route path='/books' component={BooksPage} />
+                        <Route path='/books' exact component={BooksPage} />
+                        <Route path='/books/:id' render={
+                            ({match, location, history}) => {
+                                console.log(match);
+                                console.log(location);
+                                console.log(history);
+                                const {id} = match.params;
+
+                            return <BooksItem bookId={id} />}
+                        } />
                     </Container>
                 </div>
             </Router>
